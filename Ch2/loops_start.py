d351fca78f55a506b39c2901910dc67e762b919b
pabloturati <pablo.turati@gmail.com>
2019-11-05 12:21:22 -0500

    Python3 training loops

--- Ch2/loops_start.py
+++ Ch2/loops_start.py
@@ -2,23 +2,41 @@
 # Example file for working with loops
 #
 
+
 def main():
-  x = 0
+    x = 0
 
-  # define a while loop
+    # define a while loop
+    while (x < 5):
+        print(x)
+        x = x+1
 
+    # define a for loop
+    for x in range(5, 10):
+        print(x)
 
-  # define a for loop
+    # use a for loop over a collection
+    days = ["Monday", "Tuesday", "Wednesday",
+            "Thursday", "Friday", "Satuday", "Sunday"]
+    # for day in days:
+    print(day)
 
+    # use the break statement
+    for day in days:
+        if(day == "Wednesday"):
+            break
+        print(day)
 
-  # use a for loop over a collection
-    
- 
-  # use the break and continue statements
+    # use the continue statement
+    for day in days:
+        if(day == "Friday"):
+            continue
+        print(day)
 
-
-  #using the enumerate() function to get index 
+    # using the enumerate() function to get index
+    for index, day in enumerate(days):
+        print(index, day)
 
 
 if __name__ == "__main__":
-  main()
+    main()
